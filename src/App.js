4569f29c370a56f1b55d82eb3ea4614b20dbd364
Robert Cheseldine <robert.cheseldine@gmail.com>
2020-02-08 11:46:51 -0500

    importing data via require

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+const sample = require("./data/sample.json");
 
 function App() {
-    return <div className="App">A clean template is a happy template.</div>;
+    return (
+        <div className="App">
+            A clean template is a happy template.
+            <div>{sample[0].Payee.Name}</div>
+        </div>
+    );
 }
 
 export default App;
