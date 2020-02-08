06672c96cc2e12c1865e03f97902ff174b81bd74
Robert Cheseldine <robert.cheseldine@gmail.com>
2020-02-08 13:02:49 -0500

    updating with handled state and Pagination comp value passing up

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const sample = require("./data/sample.json");
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { activePage: 0 };
+        this.state = { activePage: 1 };
     }
 
     handlePageSelect = e => {
