64b91d9524feac5596714271f5a4eb90e6527e9d
Robert Cheseldine <robert.cheseldine@gmail.com>
2020-03-19 17:33:51 -0400

    Updates

---
+++ precache-manifest.01b4e0e12f29334ad07e0c54816d1e73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eca4e6c14c03c7b2aba760574b8bf97",
+    "url": "/transaction_ui_exercise/index.html"
+  },
+  {
+    "revision": "1d2fd9320d8d52cc44ef",
+    "url": "/transaction_ui_exercise/static/css/main.e08b63df.chunk.css"
+  },
+  {
+    "revision": "fd477526beb5bb393373",
+    "url": "/transaction_ui_exercise/static/js/2.e1426fd6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/transaction_ui_exercise/static/js/2.e1426fd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d2fd9320d8d52cc44ef",
+    "url": "/transaction_ui_exercise/static/js/main.47d6cce9.chunk.js"
+  },
+  {
+    "revision": "e25200fd74f310012ef0",
+    "url": "/transaction_ui_exercise/static/js/runtime-main.b40e3709.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/technical_paymerang/precache-manifest.35059f3a36c93d225ab27a6e78fe175b.js"
+  "/transaction_ui_exercise/precache-manifest.01b4e0e12f29334ad07e0c54816d1e73.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/technical_paymerang/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/transaction_ui_exercise/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
